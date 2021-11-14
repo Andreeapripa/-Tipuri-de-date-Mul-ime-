213037698ed7d0812acce0a6273f47328d93ee4b
Andreeapripa <70636457+Andreeapripa@users.noreply.github.com>
2021-11-14 22:45:05 +0200

    Add files via upload

---
+++ problema_8_pag60.py
@@ -0,0 +1,7 @@
+a={1, 2, 4}
+b={3, 2, 7}
+print(a)
+print(b)
+print('intersectia:',a.intersection(b))
+print('Reuninea:',a.union(b))
+print('Dieferenta:',a.difference(b))
---
+++ problema_9_pag60.py
@@ -0,0 +1,7 @@
+a={'A', 'B', 'C'}
+b={'C', 'D', 'E'}
+print(a)
+print(b)
+print('Intersectia:', a.intersection(b))
+print('Reuniunea:', a.union(b))
+print('Dieferenta:', a.difference(b))
